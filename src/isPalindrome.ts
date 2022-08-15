45e54eb4fc2c592e8d5e379edce21072287cf4b4
Sercan Tuna <ssercantuna@hotmail.com>
2022-08-15 12:14:34 -0400

    done

--- src/isPalindrome.ts
+++ src/isPalindrome.ts
@@ -1,3 +1,6 @@
 function isPalindrome(word: string): boolean {
   return word.toLowerCase().split('').reverse().join('') === word.toLowerCase();
 }
+
+console.log(isPalindrome('Sercan'));
+console.log(isPalindrome('redivider'));
